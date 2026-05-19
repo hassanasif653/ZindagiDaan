cc21e1857f84647ae398b5687a0fd845ff17cb71
Hassan Asif <hassanasif653@gmail.com>
2026-05-19 10:43:12 +0500

    fixed railway production issues

--- blood bond client/src/Page/Shared/HelpDesk/HelpDesk.tsx
+++ blood bond client/src/Page/Shared/HelpDesk/HelpDesk.tsx
@@ -56,7 +56,7 @@ const HelpDesk: React.FC = () => {
   setIsTyping(true);
 
   try {
-    const res = await fetch("import.meta.env.VITE_API_URL/chatbot", {
+   const res = await fetch( `${import.meta.env.VITE_API_URL}/chatbot`,{
       method: "POST",
       headers: {
         "Content-Type": "application/json",
